8377e9456dbd6911e299fc6f69e893d43a2aab4f
luan-evangelista <luan.evieira43@gmail.com>
2021-06-09 10:52:29 -0300

    Buscando Produtos da API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,107 +1,50 @@
-import React from 'react';
-import { MdAddShoppingCart } from 'react-icons/md'
+import React, { Component } from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-    return (
-        <ProductList>
-            <li>
-                <img
-                    src="https://images.lojanike.com.br/1024x1024/produto/tenis-nike-renew-run-masculino-CK6357-002-1.jpg"
-                    alt="Tênis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$129,90</span>
+export default class Home extends Component {
+    state = {
+        products: [],
+    };
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
+    async componentDidMount() {
+        const response = await api.get('products');
 
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://images.lojanike.com.br/1024x1024/produto/tenis-nike-renew-run-masculino-CK6357-002-1.jpg"
-                    alt="Tênis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$129,90</span>
+        const data = response.data.map(products => ({
+            ...products,
+            priceFormatted: formatPrice(products.price),
+        }));
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
+        this.setState({ products: data });
+    }
 
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://images.lojanike.com.br/1024x1024/produto/tenis-nike-renew-run-masculino-CK6357-002-1.jpg"
-                    alt="Tênis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$129,90</span>
+    render() {
+        const { products } = this.state;
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
+        return (
+            <ProductList>
+                { products.map(products => (
+                    <li key={products.id}>
+                        <img
+                            src={products.image}
+                            alt={products.title}
+                        />
+                        <strong>{products.title}</strong>
+                        <span>{products.priceFormatted}</span>
 
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://images.lojanike.com.br/1024x1024/produto/tenis-nike-renew-run-masculino-CK6357-002-1.jpg"
-                    alt="Tênis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$129,90</span>
+                        <button type="button">
+                            <div>
+                                <MdAddShoppingCart size={16} color="#FFF" /> 3
+                                        </div>
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
-
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://images.lojanike.com.br/1024x1024/produto/tenis-nike-renew-run-masculino-CK6357-002-1.jpg"
-                    alt="Tênis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$129,90</span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
-
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://images.lojanike.com.br/1024x1024/produto/tenis-nike-renew-run-masculino-CK6357-002-1.jpg"
-                    alt="Tênis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>R$129,90</span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
-
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-        </ProductList>
-    );
+                            <span>ADICIONAR AO CARRINHO</span>
+                        </button>
+                    </li>
+                ))}
+            </ProductList>
+        );
+    }
 }
