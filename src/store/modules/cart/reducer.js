0b22b01b714d97dadfca93097c824ad9a1dfd5a9
luan-evangelista <luan.evieira43@gmail.com>
2021-06-09 15:52:09 -0300

    Duplicando Produto

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,13 +1,20 @@
+import produce from 'immer';
+
 export default function cart(state = [], action) {
     switch (action.type) {
         case 'ADD_TO_CART':
-            return [
-                ...state,
-                {
-                    ...action.products,
-                    amount: 1,
-                },
-            ];
+            return produce(state, draft => {
+                const productsIndex = draft.findIndex(p => p.id === action.products.id);
+
+                if (productsIndex >= 0) {
+                    draft[productsIndex].amount += 1;
+                } else {
+                    draft.push({
+                        ...action.products,
+                        amount: 1,
+                    });
+                }
+            });
         default:
             return state;
     }
